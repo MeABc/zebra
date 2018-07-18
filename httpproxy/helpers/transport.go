bb9330c10d44c5b63455acac2a7bc8d8bb4b1582
MeABc <MeABc@users.noreply.github.com>
2018-07-18 20:31:01 +0800

    transport.go: fix golint warning

--- httpproxy/helpers/transport.go
+++ httpproxy/helpers/transport.go
@@ -104,9 +104,8 @@ func CloneRequest(r *http.Request) *http.Request {
 func GetHostName(req *http.Request) string {
 	if host, _, err := net.SplitHostPort(req.Host); err == nil {
 		return host
-	} else {
-		return req.Host
 	}
+	return req.Host
 }
 
 func IsStaticRequest(req *http.Request) bool {
