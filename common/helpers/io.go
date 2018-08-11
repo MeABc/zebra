c0f13759b03d4eb89463ee7ba5e1ec8a4ad38b36
MeABc <MeABc@users.noreply.github.com>
2018-08-12 04:04:32 +0800

    io.go: Log errors during IOCopy

--- common/helpers/io.go
+++ common/helpers/io.go
@@ -1,9 +1,10 @@
 package helpers
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"sync"
-	// "github.com/cloudflare/golibs/bytepool"
 )
 
 const (
@@ -28,8 +29,41 @@ func IOCopy(dst io.Writer, src io.Reader) (written int64, err error) {
 	}
 
 	buf := bufpool.Get().([]byte)
-	written, err = io.CopyBuffer(dst, src, buf)
+	written, err = copyBuffer(dst, src, buf)
 	bufpool.Put(buf)
 
 	return written, err
+}
+
+// copyBuffer returns any write errors or non-EOF read errors, and the amount
+// of bytes written.
+func copyBuffer(dst io.Writer, src io.Reader, buf []byte) (int64, error) {
+	if len(buf) == 0 {
+		buf = make([]byte, BUFSZ)
+	}
+	var written int64
+	for {
+		nr, rerr := src.Read(buf)
+		if rerr != nil && rerr != io.EOF && rerr != context.Canceled {
+			rerr = fmt.Errorf("IOCopy: Read error during body copy: %v", rerr)
+		}
+		if nr > 0 {
+			nw, werr := dst.Write(buf[:nr])
+			if nw > 0 {
+				written += int64(nw)
+			}
+			if werr != nil {
+				return written, werr
+			}
+			if nr != nw {
+				return written, io.ErrShortWrite
+			}
+		}
+		if rerr != nil {
+			if rerr == io.EOF {
+				rerr = nil
+			}
+			return written, rerr
+		}
+	}
 }
