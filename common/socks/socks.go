7fe55194fa89b7a4046bbf7a4a39455e56a7d1af
MeABc <MeABc@users.noreply.github.com>
2018-08-12 01:56:07 +0800

    common socks: remove pool.go

--- common/socks/socks.go
+++ common/socks/socks.go
@@ -200,8 +200,7 @@ func (ln *SocksListener) Accept() (net.Conn, error) {
 func NewSocksConn(c net.Conn) (*SocksConn, *bufio.Reader, error) {
 	conn := new(SocksConn)
 	conn.Conn = c
-	//bio := bufio.NewReader(c)
-	bio := NewBufReaderFromPool(c)
+	bio := bufio.NewReader(c)
 	// Determine which SOCKS version the client is using and branch on it.
 	if version, err := socksPeekByte(bio); err != nil {
 		//conn.Close()
